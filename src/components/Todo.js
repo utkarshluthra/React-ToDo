4266dd5690ff464279d7e2abcc261539a89ad699
Utkarsh Luthra <utkarshluthra@gmail.com>
2021-10-20 00:25:25 +0530

    Added REMOVE and ADD feature

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,30 @@
-import React, { Component } from 'react'
+/* eslint-disable */
 
+import React, { Component } from 'react'
+import { Consumer } from '../context'
 export default class Todo extends Component {
+    style=()=>{
+        const {complete}=this.props.todo
+        return { textDecoration: complete ? "line-through" : "none"}
+    }
+    toggle = (id, dispatch) => {
+        dispatch({type:"TOGGLE", payload:id})
+    }
+    remove = (id, dispatch) => {
+        dispatch({type:"REMOVE", payload:id})
+    }
     render() {
         const { title } = this.props.todo
+        const { id } = this.props.todo
         return (
+            <Consumer>{value=>{
+                const {dispatch}=value 
+                return <p className="text-dark text-center p-1 bg-light border-bottom" style={this.style()}>
+                <i className="fa-solid fa-ban float-left m-1 text-danger" onClick={this.remove.bind(this, id, dispatch)}></i>{title}
+                <input type="checkbox" className="m-2 float-right" onChange={this.toggle.bind(this, id ,dispatch)}/>
+            </p>
+            }}</Consumer>
             
-            <h3 className="text-dark text-center p-1 bg-light border-bottom">
-                <i className="fas fa-times-cicles fa-sm float-left m-1 text-danger"></i>{title}
-                <input type="checkbox" className="m-2 float-right" />
-            </h3>
         )
     }
 }
--- src/context.js
+++ src/context.js
@@ -1,7 +1,28 @@
+/* eslint-disable */
+
 import React, { Component } from 'react'
 
 const Context = React.createContext()
-
+const reducer = (prevState, action)=>{
+    switch(action.type){
+        case "TOGGLE":
+            return {todos:prevState.todos.map(
+                    t=>{
+                        if(t.id===action.payload){
+                            t.complete = !(t.complete)
+                    };
+                    return t}
+                    )}
+        case "REMOVE":
+            return {todos: prevState.todos.filter(
+                todos=>todos.id !== action.payload
+            )}
+        case "ADD":
+            return {todos: [...prevState.todos, action.payload]}
+        default:
+            return prevState
+    }
+}
 export class Provider extends Component {
     state = {
         todos:[
@@ -21,7 +42,8 @@ export class Provider extends Component {
                 completed:false
             },
 
-        ]
+        ],
+        dispatch:(action)=> this.setState(prevState => reducer(prevState, action))
     }
     render() {
         return (
